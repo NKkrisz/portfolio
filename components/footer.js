946aefb15a79e4150f2d4b8ac2f4de3fbc340d12
NKkrisz <nkkrisz2005@gmail.com>
2023-12-06 08:56:31 +0100

    Hello World!

---
+++ components/footer.js
@@ -0,0 +1,15 @@
+const footerHTML = `
+    <footer>
+        <div>
+            <p>You can find me here too:</p>
+                <ul>
+                    <li><a href="https://github.com/NKkrisz"><img src="img/github.svg" alt="Github"></a></li>
+                    <li><a href="https://www.reddit.com/user/NKkrisz"><img src="img/reddit.svg" alt="Reddit"></a></li>
+                    <li><a href="https://x.com/nkkrisz"><img src="img/x.svg" alt="X"></a></li>
+                </ul>
+        </div>
+        <p>Icons made by: <a href="https://icons8.com/">Icons8</a></p>
+    </footer>
+`
+
+document.body.innerHTML += footerHTML
---
+++ components/navbar.js
@@ -0,0 +1,14 @@
+const navbarHTML = `
+    <nav>
+        <p>NKkrisz:</p>
+        <ul>
+            <li><a href="">Home</a></li>
+            <span class="seperate"></span>
+            <li><a href="">Projects</a></li>
+            <span class="seperate"></span>
+            <li><a href="">Contact</a></li>
+        </ul>
+    </nav>
+`
+
+document.body.innerHTML += navbarHTML
