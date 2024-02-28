243a70884a85565d0e19081512063bc8a43a62d8
NKkrisz <nkkrisz2005@gmail.com>
2024-02-28 09:45:45 +0100

    Page load test 2

--- scripts/navigator.js
+++ scripts/navigator.js
@@ -1,4 +1,5 @@
 async function changePage(page) {
+    page = page.toLowerCase();
     const response = await fetch(`pages/${page}.html`);
     const content = await response.text();
     document.querySelector("#container").innerHTML = content;
